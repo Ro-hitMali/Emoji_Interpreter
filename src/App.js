c16d85d0ac5ca28c70e247eb69dfe6ac168e1176
Ro-hitMali <rm4701740@gmail.com>
2022-12-17 17:08:13 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useState } from "react";
+import "./styles.css";
 
-function App() {
+const emojiDictionary = {
+  "😀": "Grinning Face",
+  "🙃": "Upside-Down Face",
+  "🤑": "Money-Mouth Face",
+  "😐":	"Neutral face",
+  "🥵":	"Hot face",
+  "👿":	"Angry face with horns",
+  "💀":	"Skull",
+  "👻":	"Ghost",
+  "💖":	"Sparking heart",
+  "🤟":	"Love you",
+  "👀":	"Eyes",
+  "🐯":	"Tiger face",
+}
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("")
+
+  function emojiInputHandler(event){
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined){
+      meaning = "we don't have this is our database"
+    }
+
+    //console.log(meaning);
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler (emoji){
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+    //console.log(emoji);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 > INSIDE OUTTT</h1>
+      <input onChange = {emojiInputHandler}/>
+      <h2>Meaning {meaning}</h2>
+      <h3> Emojis we know</h3>
+        {emojisWeKnow.map(function(emoji){
+          return <span 
+          onClick = {() => emojiClickHandler}
+          style={{fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }} key ={emoji}>{emoji}</span>;
+        }) }
     </div>
   );
 }
-
-export default App;
